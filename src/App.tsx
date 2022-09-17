f9edbbbfa1ed17daafef5d86de6c0adc8f04a288
Tanaka Kensaku <tannakaken@gmail.com>
2022-09-17 16:22:40 +0900

    arテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Euler, Vector3 } from "three";
 import { useControls } from "leva";
 import "./App.css";
 import Branch from "./branch";
+import { ARButton, XR } from "@react-three/xr";
 
 function App() {
   const { depth, branching, angle, noise } = useControls({
@@ -35,20 +36,25 @@ function App() {
   });
   return (
     <div className="App">
-      <Canvas>
-        <OrbitControls />
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Branch
-          position={new Vector3(0, 0, 0)}
-          rotation={new Euler(0, 0, 0)}
-          scale={1}
-          depth={depth}
-          branching={branching}
-          angle={(angle / 180) * Math.PI}
-          noise={noise}
-        />
-      </Canvas>
+      <Suspense fallback={<p>loading...</p>}>
+        <ARButton />
+        <Canvas>
+          <XR>
+            <OrbitControls />
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Branch
+              position={new Vector3(0, 0, 0)}
+              rotation={new Euler(0, 0, 0)}
+              scale={1}
+              depth={depth}
+              branching={branching}
+              angle={(angle / 180) * Math.PI}
+              noise={noise}
+            />
+          </XR>
+        </Canvas>
+      </Suspense>
     </div>
   );
 }
